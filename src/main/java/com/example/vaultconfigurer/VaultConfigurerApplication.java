b09f0a0ba272ded03d78e6223d62223d1c351710
Raj <the.raj.saxena@gmail.com>
2024-02-23 11:16:33 +0100

    Initializer

---
+++ src/main/java/com/example/vaultconfigurer/VaultConfigurerApplication.java
@@ -0,0 +1,13 @@
+package com.example.vaultconfigurer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VaultConfigurerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VaultConfigurerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/vaultconfigurer/VaultConfigurerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.vaultconfigurer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VaultConfigurerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
