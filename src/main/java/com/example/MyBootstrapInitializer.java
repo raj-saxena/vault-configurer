b2eb9da4aa99fda3e838b4c2df65a7a415a6b61a
Raj <the.raj.saxena@gmail.com>
2024-02-29 17:07:43 +0100

    Use application name instead of hardcoded path & cleanup

--- src/main/java/com/example/MyBootstrapInitializer.java
+++ src/main/java/com/example/MyBootstrapInitializer.java
@@ -2,14 +2,19 @@ package com.example;
 
 import org.springframework.boot.BootstrapRegistry;
 import org.springframework.boot.BootstrapRegistryInitializer;
+import org.springframework.cloud.vault.config.SecretBackendConfigurer;
 import org.springframework.cloud.vault.config.VaultConfigurer;
+import org.springframework.cloud.vault.config.VaultProperties;
 
 public class MyBootstrapInitializer implements BootstrapRegistryInitializer {
+
     @Override
     public void initialize(BootstrapRegistry registry) {
-        registry.register(
-                VaultConfigurer.class,
-                (context) -> new MyVaultSecretConfigurer()
-        );
+        registry.register(VaultConfigurer.class, context -> configurer -> {
+            var appName = context.get(VaultProperties.class).getApplicationName();
+            configurer.add("secret/%s".formatted(appName));
+            configurer.registerDefaultKeyValueSecretBackends(false);
+            configurer.registerDefaultDiscoveredSecretBackends(true);
+        });
     }
 }
